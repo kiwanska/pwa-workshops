e224bbec28a04d7d62b46278926f0e1a0dc33037
Kasia Iwańska <katarzyna.iwanska@netguru.co>
2018-02-24 12:58:33 +0100

    cache assets :unicorn:

--- src/sw.js
+++ src/sw.js
@@ -1,1 +1,21 @@
-console.log('kasia')
+const { assets } = global.serviceWorkerOption;
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('cache-v1')
+      .then((cache) => cache.addAll(assets)
+      .catch(err => console.log(err))
+  ));
+});
+
+self.addEventListener('activate', () => {
+  console.log('2')
+})
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.open('cache-v1')
+      .then((cache) => cache.match('cache-v1')
+      .catch(err => console.log(err))
+  ));
+});
